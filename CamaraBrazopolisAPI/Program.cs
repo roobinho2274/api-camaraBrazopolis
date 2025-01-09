22c9bebb10df022aa6566b5b2686ec17b0eded35
roobinho2274 <robinho2274@gmail.com>
2025-01-09 11:27:41 -0300

    Iniciando projeto com CRUD de Leis

---
+++ CamaraBrazopolisAPI/Program.cs
@@ -0,0 +1,36 @@
+using CamaraBrazopolisAPI.Data;
+using CamaraBrazopolisAPI.Middleware;
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configurar conexão com o banco
+var connectionString = builder.Configuration.GetConnectionString("Camara");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
